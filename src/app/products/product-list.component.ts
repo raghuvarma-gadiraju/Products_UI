19954a1f77ea2d7af7d4811df63cf7b4202574fc
raghuvarma-gadiraju <raghuvarma.sql@gmail.com>
2024-01-10 18:20:10 -0500

    price check

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import {IProduct} from './product'
 import {ConvertToSpacePipe} from '../shared/convert-to-space.pipe'
 import {FormsModule} from '@angular/forms'
@@ -18,11 +18,14 @@ export class ProductListComponent implements OnInit {
   imageMargin: Number = 2;
   private _listFilter : string = '';
   errorMessage: string = '';
+  finalPrice: string = ''
+  @ViewChild('myDiv') myDiv!: ElementRef;
   constructor(private productService : ProductService){
 
   }
   products: IProduct[] = []
   filteredProducts : IProduct[] = [];
+
 
   get listFilter() : string {
     return this._listFilter;
@@ -32,6 +35,9 @@ export class ProductListComponent implements OnInit {
     this._listFilter = value
     this.filteredProducts = this.performFilter(value)
   }
+
+
+
 
   performFilter(value: string) : IProduct[] {
     value = value.toLocaleLowerCase();
@@ -60,5 +66,49 @@ export class ProductListComponent implements OnInit {
  }
 
 
+ addToCart(): string {
+  let price = [];
+  let row= []
+  let tbl1 = document.getElementById('productsTable')!;
+  var checkBoxes = tbl1.getElementsByTagName("input");
+  for (var i = 0; i < checkBoxes.length; i++) {
+    if (checkBoxes[i].checked) {
+      row[i] = document.getElementsByTagName('tr')[i];
+      price.push(row[i].cells[4].innerText);
+      
+    } 
+  }
+
+  price = price.map((x)=> parseInt(x));
+
+  //find max value and apply discount
+  let max = Math.max.apply(null,price);
+
+  if (price.length >=3) {
+  for (var i = 0; i<=price.length;i++){
+    if (price[i]==max){
+        price.splice(i,1)
+    }
+  }
+ 
+  let discountedPrice = max*0.8;
+  price.push(discountedPrice)
+}
+  
+  let sum=0;
+  for (var k = 0; k< price.length; k++){
+    sum += price[k];
+  }
+
+  this.finalPrice = '';
+  return this.finalPrice = this.finalPrice + 'Total Price: ' + sum;
+  
+  
+}
+  
+  
+
+
+ 
 
 }
